66aba8917935e94930b9dd969d8cf699edce42b1
zosyk <alekcandrzosyk@gmail.com>
2018-04-08 20:15:10 +0300

    created test class for task list service

---
+++ src/test/java/com/nangasystems/tasklist/service/DefaultTaskListServiceTest.java
@@ -0,0 +1,14 @@
+package com.nangasystems.tasklist.service;
+
+import org.junit.Test;
+
+
+public class DefaultTaskListServiceTest {
+
+    private DefaultTaskListService service = new DefaultTaskListService();
+
+    @Test
+    public void name() {
+
+    }
+}
